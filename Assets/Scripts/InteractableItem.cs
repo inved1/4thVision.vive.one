2f1bbbba6ccabd6723fd16ee5362d6734c7ef140
Daniel Inversini <inved1@bfh.ch>
2016-10-04 23:41:49 +0200

    test one

---
+++ Assets/Scripts/InteractableItem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableItem : MonoBehaviour {
+
+
+    public Rigidbody rigibody;
+
+
+    private bool currentlyInteracting;
+    private WandController attachedWand;
+    private Transform interactionPoint;
+
+    private float velocityFactor = 20000f;
+    private float rotationFactor = 400f;
+   
+
+    private Vector3 posDelta;
+    private Quaternion rotDelta;
+    private float angle;
+    private Vector3 axis;
+
+	// Use this for initialization
+	void Start () {
+        rigibody = GetComponent<Rigidbody>();
+        interactionPoint = new GameObject().transform;
+        velocityFactor /= rigibody.mass;
+        rotationFactor /= rigibody.mass;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(attachedWand && currentlyInteracting)
+        {
+            posDelta = attachedWand.transform.position - interactionPoint.position;
+            this.rigibody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+
+            rotDelta = attachedWand.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+            rotDelta.ToAngleAxis(out angle, out axis);
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            this.rigibody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+        }
+	}
+
+    public void BeginInteraction(WandController wand)
+    {
+        attachedWand = wand;
+        interactionPoint.position = wand.transform.position;
+        interactionPoint.rotation = wand.transform.rotation;
+        interactionPoint.SetParent(transform, true);
+
+        currentlyInteracting = true;
+    }
+
+    public void EndInteraction(WandController wand)
+    {
+        if(wand == attachedWand)
+        {
+            attachedWand = null;
+            currentlyInteracting = false;
+        }
+    }
+    public bool IsInteraction()
+    {
+        return currentlyInteracting;
+    }
+}
---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WandController : MonoBehaviour {
+
+
+
+
+    //ref for buttons
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+
+    public  bool gripButtonDown = false;
+    public  bool gripButtonUp = false;
+    public  bool gripButtonPressed = false;
+     
+    public  bool triggerButtonDown = false;
+    public  bool triggerButtonUp = false;
+    public  bool triggerButtonPressed = false;
+
+    
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+
+    HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
+
+    private InteractableItem closestItem;
+    private InteractableItem interactingItem;
+
+	void Start () {
+
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(controller == null)
+        {
+            Debug.Log("Controller not inizialised");
+            return;
+        }
+
+
+        gripButtonDown = controller.GetPressDown(gripButton);
+        gripButtonUp = controller.GetPressUp(gripButton);
+        gripButtonPressed = controller.GetPress(gripButton);
+
+
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        triggerButtonUp = controller.GetPressUp(triggerButton);
+        triggerButtonPressed = controller.GetPress(triggerButton);
+
+        if (gripButtonDown) // wenn durch den trigger bereits collided
+        {
+            float minDistance = float.MaxValue;
+            float distance;
+            foreach(InteractableItem i in objectsHoveringOver)
+            {
+                distance = (i.transform.position - transform.position).sqrMagnitude;
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestItem = i;
+                }
+            }
+
+            interactingItem = closestItem;
+
+            if (interactingItem)
+            {
+                if (interactingItem.IsInteraction())
+                {
+                    interactingItem.EndInteraction(this);
+                }
+                interactingItem.BeginInteraction(this);
+            }
+    
+
+        }
+
+        if (gripButtonUp && interactingItem != null)
+        {
+            interactingItem.EndInteraction(this);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+        if (collidedItem)
+        {
+            objectsHoveringOver.Add(collidedItem);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+        if (collidedItem)
+        {
+            objectsHoveringOver.Remove(collidedItem);
+        }
+    }
+}
